8624e99eb3ec45fca2cc5ce6fedb8be7896935b5
Nida Chaudhry <nidachaudhry@Nidas-MacBook-Pro.local>
2017-09-11 18:45:18 -0400

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+// Local Database Configuration with Mongoose
+// mongoose.connect("mongodb://localhost/goodnews", function(error)
+// 	{if(error) throw error;
+// 	console.log("Database connected");
+// });
+
+// mLab database
+mongoose.connect("mongodb://heroku_5hq49lj8:9depouatqb4g9kvivslmtqa5qn@ds133084.mlab.com:33084/heroku_5hq49lj8", function(err) {
+	if(err) throw err;
+	console.log('database connected');
+});
+
