7cebac61edd1c612f9fdb44e28c3654453340402
Nida Chaudhry <nidachaudhry@Nidas-MacBook-Pro.local>
2017-09-12 21:05:04 -0400

    the little things..

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ var mongoose = require("mongoose");
 // 	console.log("Database connected");
 // });
 
-// mLab database
+// robo 3t database
 mongoose.connect("mongodb://heroku_5hq49lj8:9depouatqb4g9kvivslmtqa5qn@ds133084.mlab.com:33084/heroku_5hq49lj8", function(err) {
 	if(err) throw err;
 	console.log('database connected');
