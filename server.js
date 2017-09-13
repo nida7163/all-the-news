7803c5a773d35d7fd43cd28729a50c6133e9cd60
Nida Chaudhry <nidachaudhry@Nidas-MacBook-Pro.local>
2017-09-12 21:59:05 -0400

    please work on heroku

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var app = express();
 var mongoose = require('mongoose');
-db_url = process.env.MONGOHQ_URL || "mongodb://heroku_5hq49lj8:9depouatqb4g9kvivslmtqa5qn@ds133084.mlab.com:33084/heroku_5hq49lj8",
-    db = mongoose.connect(db_url),
-    Schema = mongoose.Schema;
+	db_url = process.env.MONGOHQ_URL || "mongodb://heroku_5hq49lj8:9depouatqb4g9kvivslmtqa5qn@ds133084.mlab.com:33084/heroku_5hq49lj8",
+	db = mongoose.connect(db_url),
+	schema = mongoose.Schema;
+
+//Mongoose DB Test
+var MsgSchema = new Schema({
+    date: {type: Date, default: Date.now},
+    message: String
+});
+var MsgModel = db.model("messages", MsgSchema);
+var Msg = new MsgModel();
+Msg.message = "blurgh";
+Msg.save();
 
 var app = express();
 
@@ -48,4 +58,6 @@ app.use(function(req, res) {
 app.listen(port, function() {
     console.log("The magic happens on port:" + port);
 });
+
+
 
