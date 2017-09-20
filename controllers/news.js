911b7effb453007855ef64359e256632964541b3
Nida Chaudhry <nidachaudhry@Nidas-MacBook-Pro.local>
2017-09-20 12:32:47 -0400

    heroku issues

--- controllers/news.js
+++ controllers/news.js
@@ -11,37 +11,37 @@ var Promise = require("bluebird");
 // Assign Mongoose promise
 mongoose.Promise = Promise;
 
-// Mongodb models
-var article = require("../models/article.js");
-var comments = require("../models/comments.js");
+// // Mongodb models
+// var article = require("../models/article.js");
+// var comments = require("../models/comments.js");
 
 // Website To Be Scraped
 var url = "http://www.goodnewsnetwork.org/latest-news/";
 
 // Test Route To Verify Scraping Works From Route
-router.get('/test', function(req, res) {
-    // body of the html with request
-    request(url, function(error, response, html) {
-        // load that into cheerio and save it to $ for a shorthand selector
-        var $ = cheerio.load(html);
-		var result = [];
-		$(".span6").each(function(i, element) {
-			var title = $(element).find("a").find("img").attr("title");
-			var storyLink = $(element).find("a").attr("href");
-			var imgLink = $(element).find("a").find("img").attr("src");
-			var summary = $(element).find(".td-post-text-excerpt").text();
-			summary = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-			result.push({ 
-				Title: title,
-				Story: storyLink,
-				Link: imgLink,
-				Summary: summary
-			});
-		});
-		console.log(result);
-		res.send(result);
-    });
-});
+// router.get('/test', function(req, res) {
+//     // body of the html with request
+//     request(url, function(error, response, html) {
+//         // load that into cheerio and save it to $ for a shorthand selector
+//         var $ = cheerio.load(html);
+// 		var result = [];
+// 		$(".span6").each(function(i, element) {
+// 			var title = $(element).find("a").find("img").attr("title");
+// 			var storyLink = $(element).find("a").attr("href");
+// 			var imgLink = $(element).find("a").find("img").attr("src");
+// 			var summary = $(element).find(".td-post-text-excerpt").text();
+// 			summary = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+// 			result.push({ 
+// 				Title: title,
+// 				Story: storyLink,
+// 				Link: imgLink,
+// 				Summary: summary
+// 			});
+// 		});
+// 		console.log(result);
+// 		res.send(result);
+//     });
+// });
 
 // Default route renders the index handlebars view
 router.get('/', function(req, res){
@@ -49,87 +49,87 @@ router.get('/', function(req, res){
 });
 
 // Scrape the website and assign stories to the database. Checks to verify story has not been added previously.
-router.get('/scrape', function(req, res){
-    request(url, function(error, response, html) {	
-        var $ = cheerio.load(html);
-		var result = [];
-		// Scrape website
-		$(".span6").each(function(i, element) {
-		    var title = $(element).find("a").find("img").attr("title");
-		    var imgLink = $(element).find("a").find("img").attr("src");
-		    var storyLink = $(element).find("a").attr("href");
-		    var summary = $(element).find(".td-post-text-excerpt").text();
-		    summary = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
-			result[i] = ({ 
-				title: title,
-				imgLink: imgLink,
-				storyLink: storyLink,
-				summary: summary
-			});	
-			// Check database to see if story saved previously to database
-			article.findOne({'title': title}, function(err, articleRecord) {
-				if(err) {
-					console.log(err);
-				} else {
-					if(articleRecord == null) {
-						article.create(result[i], function(err, record) {
-							if(err) throw err;
-							console.log("Record Added");
-						});
-					} else {
-						console.log("No Record Added");
-					}					
-				}
-			});	
-		});
-    });	
-});
+// router.get('/scrape', function(req, res){
+//     request(url, function(error, response, html) {	
+//         var $ = cheerio.load(html);
+// 		var result = [];
+// 		// Scrape website
+// 		$(".span6").each(function(i, element) {
+// 		    var title = $(element).find("a").find("img").attr("title");
+// 		    var imgLink = $(element).find("a").find("img").attr("src");
+// 		    var storyLink = $(element).find("a").attr("href");
+// 		    var summary = $(element).find(".td-post-text-excerpt").text();
+// 		    summary = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+// 			result[i] = ({ 
+// 				title: title,
+// 				imgLink: imgLink,
+// 				storyLink: storyLink,
+// 				summary: summary
+// 			});	
+// 			// Check database to see if story saved previously to database
+// 			article.findOne({'title': title}, function(err, articleRecord) {
+// 				if(err) {
+// 					console.log(err);
+// 				} else {
+// 					if(articleRecord == null) {
+// 						article.create(result[i], function(err, record) {
+// 							if(err) throw err;
+// 							console.log("Record Added");
+// 						});
+// 					} else {
+// 						console.log("No Record Added");
+// 					}					
+// 				}
+// 			});	
+// 		});
+//     });	
+// });
 
 // Get all current article in database
-router.get('/article', function(req, res){
-	article.find().sort({ createdAt: -1 }).exec(function(err, data) { 
-		if(err) throw err;
-		res.json(data);
-	});
-});
+// router.get('/article', function(req, res){
+// 	article.find().sort({ createdAt: -1 }).exec(function(err, data) { 
+// 		if(err) throw err;
+// 		res.json(data);
+// 	});
+// });
 
 // Get all comments for one article
-router.get('/comments/:id', function(req, res){
-	comments.find({'articleId': req.params.id}).exec(function(err, data) {
-		if(err) {
-			console.log(err);
-		} else {
-			res.json(data);
-		}	
-	});
-});
+// router.get('/comments/:id', function(req, res){
+// 	comments.find({'articleId': req.params.id}).exec(function(err, data) {
+// 		if(err) {
+// 			console.log(err);
+// 		} else {
+// 			res.json(data);
+// 		}	
+// 	});
+// });
 
-// Add comment for article
-router.post('/addcomment/:id', function(req, res){
-	console.log(req.params.id+' '+req.body.comment);
-	comments.create({
-		articleId: req.params.id,
-		name: req.body.name,
-		comment: req.body.comment
-	}, function(err, docs){    
-		if(err){
-			console.log(err);			
-		} else {
-			console.log("New Comment Added");
-		}
-	});
-});
+// // Add comment for article
+// router.post('/addcomment/:id', function(req, res){
+// 	console.log(req.params.id+' '+req.body.comment);
+// 	comments.create({
+// 		articleId: req.params.id,
+// 		name: req.body.name,
+// 		comment: req.body.comment
+// 	}, function(err, docs){    
+// 		if(err){
+// 			console.log(err);			
+// 		} else {
+// 			console.log("New Comment Added");
+// 		}
+// 	});
+// });
 
-// Delete comment for article
-router.get('/deletecomment/:id', function(req, res){
-	console.log(req.params.id)
-	comments.remove({'_id': req.params.id}).exec(function(err, data){
-		if(err){
-			console.log(err);
-		} else {
-			console.log("Comment deleted");
-		}
-	})
-});
+// // Delete comment for article
+// router.get('/deletecomment/:id', function(req, res){
+// 	console.log(req.params.id)
+// 	comments.remove({'_id': req.params.id}).exec(function(err, data){
+// 		if(err){
+// 			console.log(err);
+// 		} else {
+// 			console.log("Comment deleted");
+// 		}
+// 	})
+// });
 
 module.exports = router;
